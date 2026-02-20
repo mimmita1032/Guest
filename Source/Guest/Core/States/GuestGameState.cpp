54e3e0b47b64165e13737d98b21dcb214bdf778d
Gitak Kim <alaalxk@gmail.com>
2026-02-20 20:13:59 +0900

    feat : add log categories and macros for GCharacter, GUI, GItem, and GSystem

--- Source/Guest/Core/States/GuestGameState.cpp
+++ Source/Guest/Core/States/GuestGameState.cpp
@@ -3,8 +3,11 @@
 
 #include "Guest/Core/States/GuestGameState.h"
 
+#include "Guest/Utils/GLog.h"
+
 AGuestGameState::AGuestGameState()
 {
 	// 초기 상태 설정
 	UE_LOG(LogTemp, Log, TEXT("게임 스테이트 초기화 완료"));
+	G_LOG(TEXT("test"))
 }
