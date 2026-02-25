2ab4bb7aafd7e234aac8d011699f036efb48fddb
Gitak Kim <alaalxk@gmail.com>
2026-02-26 00:27:40 +0900

    feat : add GItemData class for item management

---
+++ Source/Guest/Data/DataAssets/GItemData.h
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2026 Anything Left Behind?. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GItemData.generated.h"
+
+UCLASS(BlueprintType)
+class GUEST_API UGItemData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// 아이템 고유 ID (데이터 테이블 연동 시 사용)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ItemInfo")
+	FName ItemID;
+
+	// UI 및 화면에 표시될 아이템 이름
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ItemInfo")
+	FText ItemName;
+
+	// 아이템이 속한 현실 세계의 연도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Narrative")
+	int32 OriginYear;
+
+	// 플레이어가 아이템을 관찰할 때 읽을 수 있는 사연/텍스트
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Narrative", meta = (MultiLine = true))
+	FText Description;
+
+	// 에디터에서 보여질 아이템의 3D 메쉬
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Visual")
+	TObjectPtr<class UStaticMesh> ItemMesh;
+};
