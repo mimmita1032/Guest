514f5467cac65facb1d639c10f5431683f5f91b8
Gitak Kim <alaalxk@gmail.com>
2026-03-12 07:24:30 +0900

    feat: 시공간에 연동되어 실시간 회전하는 태양 액터(GSpacetimeSun) 구현
    - 변경 동기 및 목적
      GSpacetimeSubsystem의 전역 시간(WorldTime) 흐름을 게임 월드의 시각적 환경(Lighting)에 즉각적으로 반영하기 위함.
    - 기술적 해결 방법
      AActor를 상속받은 AGSpacetimeSun 클래스에 UDirectionalLightComponent를 부착하고 Movable 모빌리티를 설정. BeginPlay 시점에서 서브시스템의 OnTimeChanged 델리게이트에 AddDynamic으로 OnTimeUpdated 함수를 교차 바인딩.
    - 주요 특징
      CurrentHour(0~24)를 선형 방정식((Hour / 24) * -360 + 90)을 통해 FRotator의 Pitch 값으로 변환 및 SetWorldRotation 적용. 틱(Tick)을 사용하지 않는 이벤트 기반(Event-Driven) 최적화 회전 로직 완성.

---
+++ Source/Guest/Environment/GSpacetimeSun.cpp
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2026 Anything Left Behind?. All rights reserved.
+
+
+#include "GSpacetimeSun.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Guest/Subsystem/GSpacetimeSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+
+AGSpacetimeSun::AGSpacetimeSun()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SunLight = CreateDefaultSubobject<UDirectionalLightComponent>(TEXT("SunLight"));
+	RootComponent = SunLight;
+	
+	SunLight->SetMobility(EComponentMobility::Movable);
+}
+
+void AGSpacetimeSun::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (UGameInstance* GI = GetGameInstance())
+	{
+		if (UGSpacetimeSubsystem* SpacetimeSS = GI->GetSubsystem<UGSpacetimeSubsystem>())
+		{
+			SpacetimeSS->OnTimeChanged.AddDynamic(this, &AGSpacetimeSun::OnTimeUpdated);
+			
+			OnTimeUpdated(SpacetimeSS->GetCurrentHour());
+		}
+	}
+}
+
+void AGSpacetimeSun::OnTimeUpdated(float CurrentHour)
+{
+	// 언리얼의 Pitch(Y축 회전) 기준:
+	// -90 = 직각으로 내려쬠 (정오, 12시)
+	// 0 = 지평선 (일출 6시)
+	// -180 = 지평선 반대편 (일몰 18시)
+	// 90 = 직각으로 올려쬠 (자정, 0시/24시 - 바닥 아래)
+	
+	// 24시간을 360도로 변환하는 선형 보간
+	float CalculatedPitch = (CurrentHour / 24.0f) * -360.0f + 90.0f;
+
+	// 계산된 Pitch 값으로 새로운 회전값 생성 (Yaw와 Roll은 0으로 고정)
+	FRotator NewRotation = FRotator(CalculatedPitch, 0.0f, 0.0f);
+	
+	// 라이트 컴포넌트에 새로운 회전값 적용
+	SunLight->SetWorldRotation(NewRotation);
+}
+
---
+++ Source/Guest/Environment/GSpacetimeSun.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2026 Anything Left Behind?. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GSpacetimeSun.generated.h"
+
+class UDirectionalLightComponent;
+
+UCLASS()
+class GUEST_API AGSpacetimeSun : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AGSpacetimeSun();
+
+protected:
+	virtual void BeginPlay() override;
+
+	// 서브시스템의 시간이 변할 때 호출될 콜백 함수
+	UFUNCTION()
+	void OnTimeUpdated(float CurrentHour);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Light")
+	TObjectPtr<UDirectionalLightComponent> SunLight;
+};
