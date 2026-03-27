56d515e3962519411620f9002cb7d228115f44b7
Gitak Kim <alaalxk@gmail.com>
2026-03-27 21:14:40 +0900

    feat: 시간 변화 감지를 위한 인터페이스(GTimeListenerInterface) 구현
    IGTimeListenerInterface를 선언하고, 시/분 정보를 매개변수로 받는 OnTimeChanged 순수 가상 함수를 포함

---
+++ Source/Guest/Interfaces/GTimeListenerInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Copyright (c) 2026 Anything Left Behind?. All rights reserved.
+
+
+#include "GTimeListenerInterface.h"
+
+
+// Add default functionality here for any IGTimeListenerInterface functions that are not pure virtual.
---
+++ Source/Guest/Interfaces/GTimeListenerInterface.h
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2026 Anything Left Behind?. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GTimeListenerInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UGTimeListenerInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ *  시간에 따라 상태가 변해야하는 액터들을 위한 인터페이스
+ */
+class GUEST_API IGTimeListenerInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void OnTimeChanged(int32 Hours, int32 Minutes) = 0;
+};
