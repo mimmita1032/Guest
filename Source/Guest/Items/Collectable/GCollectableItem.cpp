e32f9a1fe1f2a6582b4f61f3c4f6395ed5e3d413
GeonRyoung <rjsfud7467@naver.com>
2026-03-28 23:29:24 +0900

    feat: add GCollectableItem for colletion logic

---
+++ Source/Guest/Items/Collectable/GCollectableItem.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2026 Anything Left Behind?. All rights reserved.
+
+
+#include "GCollectableItem.h"
+#include "Guest/Data/DataAssets/GItemData.h"
+#include "Guest/Utils/GLog.h"
+
+
+AGCollectableItem::AGCollectableItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AGCollectableItem::Interact(AActor* Interactor)
+{
+	Super::Interact(Interactor);
+	
+	if (GetItemData()) return;
+	
+	G_LOG(TEXT("수집형 아이템 [%s]이(가) 인벤토리에 추가되었습니다. (주체: %s"), *GetItemData()->ItemName.ToString(), *Interactor->GetName());
+	
+	// [TODO] 인벤토리에 아이템 추가
+	
+	// 임시 삭제 
+	Destroy();
+}
+
