37ebbb888aa0a60b7b920982e59fd61942b2bd5e
Gitak Kim <alaalxk@gmail.com>
2026-04-09 00:34:07 +0900

    fix : 상호작용 입력바인딩 trigger->start로 변경

--- Source/Guest/Characters/Player/GuestCharacter.cpp
+++ Source/Guest/Characters/Player/GuestCharacter.cpp
@@ -151,7 +151,7 @@ void AGuestCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 
 		if (InteractAction)
 		{
-			EIC->BindAction(InteractAction, ETriggerEvent::Triggered, this, &AGuestCharacter::OnInteract);
+			EIC->BindAction(InteractAction, ETriggerEvent::Started, this, &AGuestCharacter::OnInteract);
 		} 
 
 		if (IA_DigicamControl)
