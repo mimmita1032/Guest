472a0727ca98bf4347007f00b3371b9d4c0a0e9c
SEUNGRAEM <ksl0718@nate.com>
2026-04-12 15:27:32 +0900

    feat - save 헤더 작성

---
+++ Source/Guest/Save/GuestSaveGame.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "GuestSaveGame.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FGuestPlayerWorldState
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save")
+	FVector Location = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save")
+	FRotator Rotation = FRotator::ZeroRotator;
+};
+
+
+UCLASS()
+class GUEST_API UGuestSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+	public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Save|Meta")
+	int32 SaveVersion = 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save|Player")
+	FGuestPlayerWorldState PlayerWorld;
+};
