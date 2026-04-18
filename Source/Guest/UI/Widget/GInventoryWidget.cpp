b6d5fb9e5d373cc85ed57be88cbb92dfd1081dfa
Gitak Kim <alaalxk@gmail.com>
2026-04-19 00:29:49 +0900

    feat: Common UI 기반 인벤토리 베이스 위젯 클래스 구현

---
+++ Source/Guest/UI/Widget/GInventoryWidget.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2026 Anything Left Behind?. All rights reserved.
+
+
+#include "GInventoryWidget.h"
+#include "Guest/Components/CharacterComponents/GInventoryComponent.h"
+#include "Guest/Utils/GLog.h"
+
+void UGInventoryWidget::SetInventoryComponent(UGInventoryComponent* InComponent)
+{
+	if (InComponent)
+	{
+		InventoryComponent = InComponent;
+		
+		InventoryComponent->OnInventoryChanged.AddDynamic(this, &UGInventoryWidget::OnRefreshInventory);
+		
+		UE_LOG(LogGSystem, Log, TEXT("위젯에 인벤토리 컴포넌트 연결 및 델리게이트 바인딩 완료~"));
+	}
+}
+
+void UGInventoryWidget::NativeOnActivated()
+{
+	Super::NativeOnActivated();
+
+	OnRefreshInventory();
+}
+
+void UGInventoryWidget::NativeDestruct()
+{
+	if (InventoryComponent)
+	{
+		InventoryComponent->OnInventoryChanged.RemoveAll(this);
+	}
+
+	Super::NativeDestruct();
+}
---
+++ Source/Guest/UI/Widget/GInventoryWidget.h
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2026 Anything Left Behind?. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "GInventoryWidget.generated.h"
+
+class UGInventoryComponent;
+
+UCLASS()
+class GUEST_API UGInventoryWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Inventory|UI")
+	void SetInventoryComponent(UGInventoryComponent* InComponent);
+
+protected:
+	virtual void NativeOnActivated() override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Inventory|UI")
+	void OnRefreshInventory();
+
+	virtual void NativeDestruct() override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Inventory|UI")
+	TObjectPtr<UGInventoryComponent> InventoryComponent;
+};
