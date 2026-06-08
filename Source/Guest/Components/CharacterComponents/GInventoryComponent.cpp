a38c80abfa7e091d0b9b6df1ce5ce8bbd1926e98
Gitak Kim <alaalxk@gmail.com>
2026-06-08 18:21:40 +0900

    test: PR Actions 연동 테스트 2

--- Source/Guest/Components/CharacterComponents/GInventoryComponent.cpp
+++ Source/Guest/Components/CharacterComponents/GInventoryComponent.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2026 Anything Left Behind?. All rights reserved.
+// test: PR Actions 연동 테스트 2
 
 #include "GInventoryComponent.h"
 #include "Guest/Items/Fragments/GItemFragmentInventory.h"
